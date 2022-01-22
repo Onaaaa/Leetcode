697206ff430b9d48a33b6b0efdb4838f283fa8a6
Onaaaa <411125871@qq.com>
2022-01-22 16:48:42 +0800

    First Commit

---
+++ LearnGitDemo/LearnGitDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnGitDemo
+//
+//  Created by 小锅同学 on 2022/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearnGitDemo/LearnGitDemo/LearnGitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnGitDemoApp.swift
+//  LearnGitDemo
+//
+//  Created by 小锅同学 on 2022/1/22.
+//
+
+import SwiftUI
+
+@main
+struct LearnGitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
